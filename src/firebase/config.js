030e2c23e767b05f23434688e21e468e1e118989
Patrick Noone <patricknoone@gmail.com>
2025-09-25 02:28:38 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,17 +3,16 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// --- MOCK CONFIG (WILL BE REPLACED BY HOSTING ENVIRONMENT) ---
-const firebaseConfig = typeof __firebase_config !== 'undefined'
-    ? JSON.parse(__firebase_config)
-    : {
-        apiKey: "AIza...",
-        authDomain: "...",
-        projectId: "...",
-        storageBucket: "...",
-        messagingSenderId: "...",
-        appId: "..."
-    };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBscGGCflREDZxCTb_wB4oNzog0BLPItxg",
+  authDomain: "bmsys-bd9a0.firebaseapp.com",
+  projectId: "bmsys-bd9a0",
+  storageBucket: "bmsys-bd9a0.firebasestorage.app",
+  messagingSenderId: "777990216211",
+  appId: "1:777990216211:web:1fbd88d92729431de5fde6",
+  measurementId: "G-Y9JHPKT1R7"
+};
 
 export const appId = typeof __app_id !== 'undefined' ? __app_id : 'default-app-id';
 
